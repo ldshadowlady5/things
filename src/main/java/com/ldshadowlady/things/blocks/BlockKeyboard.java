4859fc350d55bd85745785f159a18ca85b574439
Paul Fulham <paul.fulham0@gmail.com>
2020-07-06 02:26:11 -0700

    Start keyboard playability

--- src/main/java/com/ldshadowlady/things/blocks/BlockKeyboard.java
+++ src/main/java/com/ldshadowlady/things/blocks/BlockKeyboard.java
@@ -12,8 +12,12 @@ import net.minecraft.state.StateContainer;
 import net.minecraft.util.Direction;
 import net.minecraft.util.Hand;
 import net.minecraft.util.SoundCategory;
+import net.minecraft.util.SoundEvents;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.BlockRayTraceResult;
+import net.minecraft.util.math.MathHelper;
+import net.minecraft.util.math.RayTraceResult;
+import net.minecraft.util.math.Vec3d;
 import net.minecraft.util.math.shapes.ISelectionContext;
 import net.minecraft.util.math.shapes.VoxelShape;
 import net.minecraft.world.IBlockReader;
@@ -47,6 +51,7 @@ public class BlockKeyboard extends HorizontalBlock {
         return this.getDefaultState().with(HORIZONTAL_FACING, context.getPlacementHorizontalFacing().getOpposite().getOpposite());
     }
 
+    @Override
     protected void fillStateContainer(StateContainer.Builder<Block, BlockState> state) {
         state.add(HORIZONTAL_FACING);
     }
@@ -66,9 +71,30 @@ public class BlockKeyboard extends HorizontalBlock {
         }
     }
 
-    public boolean onBlockActivated(BlockState state, World worldIn, BlockPos pos, PlayerEntity player, Hand handIn, BlockRayTraceResult hit) {
-        this.playSound(player, worldIn, pos);
-        worldIn.addParticle(ParticleTypes.NOTE, pos.getX() + 0.5, pos.getY() + 0.3, pos.getZ() + 0.5, 0.0D, 0.0D, 0.0D);
+    @Override
+    public boolean onBlockActivated(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand handIn, BlockRayTraceResult hit) {
+        if (!world.isRemote) {
+            Vec3d start = player.getEyePosition(1.0F);
+            Vec3d end = start.add(player.getLook(1.0F).scale(2.0D));
+            Direction facing = state.get(HORIZONTAL_FACING);
+            VoxelShape keys = VoxelShapeUtils.rotateHorizontal(Block.makeCuboidShape(0.5D, 0.0D, 8.0D, 15.5D, 1.0D, 11.5D), facing);
+            BlockRayTraceResult result = keys.rayTrace(start, end, pos);
+            if (result != null && result.getType() == RayTraceResult.Type.BLOCK) {
+                Vec3d p = result.getHitVec();
+                double lx = facing == Direction.NORTH || facing == Direction.SOUTH ? p.x - pos.getX() : p.z - pos.getZ();
+                if (facing == Direction.SOUTH || facing == Direction.WEST) lx = 1.0D - lx;
+                double ly = facing == Direction.WEST || facing == Direction.EAST ? p.x - pos.getX() : p.z - pos.getZ();
+                if (facing == Direction.NORTH || facing == Direction.WEST) ly = 1.0D - ly;
+                double wx = (lx - 0.5D / 16.0D) / 15.0D * 16.0D;
+                double bx = ((lx - 0.5D / 16.0D) / 15.0D * 16.0D - 0.5D / 16.0D) * 16.0D;
+                double ky = (ly - 4.5D / 16.0D) / 3.5D * 16.0D;
+                int keyIndex = MathHelper.clamp((int) (wx * 14.0D), 0, 13);
+                int note = new int[] { -1, 1, 3, 5, 6, 8, 10, 11, 13, 15, 17, 18, 20, 22 }[keyIndex];
+                if (note != -1) {
+                    world.addBlockEvent(pos, this, 0, note);
+                }
+            }
+        }
         return true;
     }
 
@@ -76,4 +102,10 @@ public class BlockKeyboard extends HorizontalBlock {
         world.playSound(player, pos, SoundList.KEYBOARD_SOUND.get(), SoundCategory.BLOCKS, 1F, 1F);
     }
 
+    @Override
+    public boolean eventReceived(BlockState state, World worldIn, BlockPos pos, int id, int param) {
+        worldIn.playSound(null, pos, SoundEvents.BLOCK_NOTE_BLOCK_PLING, SoundCategory.BLOCKS, 1.0F, (float) Math.pow(2.0D, (param - 12) / 12.0D));
+        worldIn.addParticle(ParticleTypes.NOTE, pos.getX() + 0.5D, pos.getY() + 0.3D, pos.getZ() + 0.5D, param / 24.0D, 0.0D, 0.0D);
+        return true;
+    }
 }
